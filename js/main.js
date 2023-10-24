b359c4446edf47e2a041143b602ca5e7ea057d3e
Diana952 <gadzhievadiana99@gmail.com>
2023-10-24 16:45:29 +0700

    practical 6

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
         product: "Socks",
         image: "../assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -17,30 +17,19 @@ let app = new Vue({
                 variantColor: 'blue',
                 variantImage: "../assets/vmSocks-blue-onWhite.jpg",
             }
-         ],
-         
-         cart: 0 
-         
+        ],
+        cart: 0
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-
-        subtractionToCart(){
-            if(this.cart){
-                this.cart -=1;
-            }
-        },
-
         updateProduct(variantImage) {
             this.image = variantImage
-         }
-     },
-
-   
-     
-     
+        }
+    }
  })
+ 
+ 
  
  
